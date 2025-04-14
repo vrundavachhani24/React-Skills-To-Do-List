ea64e13ff59f1a753ede04f212830f63b92c57c3
vrunda <vachhanivrunda24@gmail.com>
2025-04-14 18:44:34 +0530

    validation add and design fix

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,16 @@ function App() {
       alert('Please enter your Skill')
       return;
     }
+    if (selectvalue === 'Select Category') {
+      alert('Please Select Your Category')
+      return;
+    }
     const duplicatValue = addData.find((ele) => ele.skill === skill)
     if (duplicatValue) {
       alert('Please enter your different Skill')
       return;
     }
-    const obj = { id: uuidv4(), skill , category:selectvalue}
+    const obj = { id: uuidv4(), skill, category: selectvalue }
     setAddData([...addData, obj])
     setSkill('')
     setIsDelete(false)
@@ -62,7 +66,7 @@ function App() {
   }
 
   const updateTask = () => {
-    if(skill === '') {
+    if (skill === '') {
       alert('Please Enter Your Skill')
       return;
     }
@@ -72,7 +76,7 @@ function App() {
     }
     const updatedData = addData.map((ele) => {
       if (ele.id === editid) {
-        return { id: ele.id, skill , category:selectvalue }
+        return { id: ele.id, skill, category: selectvalue }
       }
       return ele;
     })
@@ -102,10 +106,10 @@ function App() {
   }
 
   return (
-    <div className='text-center rounded-3xl text-white bg-linear-60 from-purple-600 to-pink-600 shadow-xl shadow-gray-400 mt-5 p-5 w-lg place-self-center'>
+    <div className='text-center rounded-3xl text-white bg-linear-30 from-blue-800 to-gray-300 shadow-xl shadow-gray-400 mt-5 p-5 w-lg place-self-center'>
       <div className='p-3'>
         <div className='flex justify-between'>
-          <h1 className='text-4xl'>Skills</h1>
+          <h1 className='text-4xl mt-2'>Skills</h1>
 
           <Modal opened={isaddData} onClose={() => setisAddData(false)} title={'Add Skills'}>
             <div>
@@ -117,9 +121,9 @@ function App() {
           </Modal>
 
           <div>
-            <Button variant="default" onClick={() => { setisAddData(true) }} className='shadow-xl'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <button onClick={() => { setisAddData(true) }} className='bg-white text-black p-4 rounded cursor-pointer hover:shadow-sm shadow-white '><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-            </svg></Button>
+            </svg></button>
           </div>
         </div>
 
@@ -130,7 +134,7 @@ function App() {
         </div>
 
         <Modal opened={isDelete} onClose={() => setIsDelete(false)} title={'Are you sure?'}>
-          <Button variant="filled" onClick={handleDeleteTask} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)">save</Button>
+          <Button variant="filled" onClick={handleDeleteTask} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)">Save</Button>
           <Button variant="filled" onClick={() => setIsDelete(false)} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel</Button>
         </Modal>
 
@@ -142,22 +146,23 @@ function App() {
         </Modal>
       </div>
       {
-        categoryData.map((ele) => {
-          return (
-            <div key={ele.id} className='p-2 mt-5 flex items-center justify-between bg-linear-60 from-purple-400 to-pink-400 shadow-xl'>{ele.skill}
-              <div>
-                <Button variant="filled" onClick={() => { handleEditTask(ele?.id) }} className='ms-3' color="rgba(5, 158, 33, 1)"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                </svg></Button>
-                <Button variant="filled" onClick={() => { handleDeleteClick(ele?.id) }} className='ms-3' color="rgba(235, 30, 30, 1)"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                </svg>
-                </Button>
+        categoryData.length === 0 ? (<div className="mt-5 text-xl text-white font-bold">No Data Found</div>)
+          : (categoryData.map((ele) => {
+            return (
+              <div key={ele.id} className='p-2 mt-5 flex items-center justify-between bg-linear-60 from-blue-900 to-gray-400 shadow-xl'>{ele.skill}
+                <div>
+                  <Button variant="filled" onClick={() => { handleEditTask(ele?.id) }} className='ms-3' color="rgba(5, 158, 33, 1)"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                  </svg></Button>
+                  <Button variant="filled" onClick={() => { handleDeleteClick(ele?.id) }} className='ms-3' color="rgba(235, 30, 30, 1)"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                  </svg>
+                  </Button>
+                </div>
               </div>
-            </div>
-          )
-        })
+            )
+          }))
       }
     </div>
 
