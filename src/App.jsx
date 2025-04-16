c871abf69b79a87fb558ce5311e2208b1b29989b
vrunda <vachhanivrunda24@gmail.com>
2025-04-16 17:01:27 +0530

    Active button in add skills

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,19 @@ function App() {
   const [ActiveAll, setActiveAll] = useState('All')
 
   useEffect(() => {
+    const storedData = localStorage.getItem('skillsData')
+    if (storedData) {
+      const parsedData = JSON.parse(storedData)
+      setAddData(parsedData)
+      setCategoryData(parsedData)
+    }
+  }, [])
+
+  useEffect(() => {
     setCategoryData(addData)
+    localStorage.setItem('skillsData', JSON.stringify(addData))
   }, [addData])
+
 
   const handleTask = (event) => {
     setSkill(event.target.value)
@@ -44,7 +55,7 @@ function App() {
     setSkill('')
     setIsDelete(false)
     setisAddData(false)
-    setSelectValue('Select Category')
+    setSelectValue(ActiveAll ? ActiveAll:'Select Category')
   }
 
   const handleDeleteClick = (id) => {
@@ -71,7 +82,7 @@ function App() {
       alert('Please Enter Your Skill')
       return;
     }
-    if (selectvalue === 'Select Category') {  
+    if (selectvalue === 'Select Category') {
       alert('Please Select Your Category')
       return;
     }
@@ -89,15 +100,17 @@ function App() {
   console.log(addData)
 
   const filterDataByCategory = (event) => {
-    const categoryName = event.target.name;
+    const categoryName = event.target.name
     if (categoryName === 'All') {
       setCategoryData(addData)
     } else {
-      const filteredData = addData.filter((ele) => ele.category === categoryName);  
+      const filteredData = addData.filter((ele) => ele.category === categoryName)
       setCategoryData(filteredData)
     }
+    setSelectValue(categoryName)
     setActiveAll(categoryName)
   }
+
 
   const handleModelCancel = () => {
     setSkill('')
@@ -109,47 +122,47 @@ function App() {
 
   return (
     <div className='max-w-3xl w-lg mx-auto text-center rounded-3xl text-white bg-linear-30 from-blue-800 to-gray-300 shadow-xl shadow-gray-400 mt-5 p-5 place-self-center m-5'>
-        <div className='flex justify-between'>
-          <h1 className='text-4xl mt-2'>Skills</h1>
+      <div className='flex justify-between'>
+        <h1 className='text-4xl mt-2'>Skills</h1>
 
-          <Modal opened={isaddData} onClose={() => handleModelCancel()} title={'Add Skills'}>
-            <div>
-              <Input placeholder="Add Your Skills" value={skill} onChange={handleTask} />
-              <NativeSelect value={selectvalue} onChange={(ele) => { setSelectValue(ele.target.value) }} data={['Select Category', 'Frontend', 'Backend']} className='mt-3' />
-              <Button variant="filled" onClick={handleTaskAdd} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)"> Add Data</Button>
-              <Button variant="filled" onClick={handleModelCancel} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel Data</Button>
-            </div>
-          </Modal>
-
+        <Modal opened={isaddData} onClose={() => handleModelCancel()} title={'Add Skills'}>
           <div>
-            <button onClick={() => { setisAddData(true) }} className='bg-white text-black p-4 rounded cursor-pointer hover:shadow-sm shadow-white '><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-            </svg></button>
+            <Input placeholder="Add Your Skills" value={skill} onChange={handleTask} />
+            <NativeSelect value={selectvalue} onChange={(ele) => { setSelectValue(ele.target.value) }} data={['Select Category', 'Frontend', 'Backend']} className='mt-3' />
+            <Button variant="filled" onClick={handleTaskAdd} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)"> Add Data</Button>
+            <Button variant="filled" onClick={handleModelCancel} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel Data</Button>
           </div>
-        </div>
-
-        <div className='flex gap-10 mt-8 items-center justify-center'>
-          <button name='All' onClick={filterDataByCategory} className={`shadow-sm shadow-white p-3 rounded cursor-pointer ${ActiveAll === "All" ? "bg-blue-900" : "bg-indigo-700"}`}>All</button>
-          <button name='Frontend' onClick={filterDataByCategory} className={`shadow-sm shadow-white p-3 rounded cursor-pointer ${ActiveAll === "Frontend" ? "bg-blue-900" : "bg-indigo-700"}`}>Frontend</button>
-          <button name='Backend' onClick={filterDataByCategory} className={`shadow-sm shadow-white p-3 rounded cursor-pointer ${ActiveAll === "Backend" ? "bg-blue-900" : "bg-indigo-700"}`}>Backend</button>
-        </div>
-
-        <Modal opened={isDelete} onClose={() => setIsDelete(false)} title={'Are you sure?'}>
-          <Button variant="filled" onClick={handleDeleteTask} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)">Save</Button>
-          <Button variant="filled" onClick={() => setIsDelete(false)} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel</Button>
         </Modal>
 
-        <Modal opened={iseditData} onClose={handleModelCancel} title={'Edit Your Skill'}>
-          <Input placeholder="Add Your Skills" value={skill} onChange={handleTask} name='value' />
-          <NativeSelect value={selectvalue} onChange={(ele) => { setSelectValue(ele.target.value) }} data={['Frontend', 'Backend']} className='mt-3' />
-          <Button variant="filled" onClick={updateTask} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)">Update</Button>
-          <Button variant="filled" onClick={handleModelCancel} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel</Button>
-        </Modal>
+        <div>
+          <button onClick={() => { setisAddData(true) }} className='bg-white text-black p-4 rounded cursor-pointer hover:shadow-sm shadow-white '><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+          </svg></button>
+        </div>
+      </div>
+
+      <div className='flex gap-10 mt-8 items-center justify-center'>
+        <button name='All' onClick={filterDataByCategory} className={`shadow-sm shadow-white p-3 rounded cursor-pointer ${ActiveAll === "All" ? "bg-blue-900" : "bg-indigo-700"}`}>All</button>
+        <button name='Frontend' onClick={filterDataByCategory} className={`shadow-sm shadow-white p-3 rounded cursor-pointer ${ActiveAll === "Frontend" ? "bg-blue-900" : "bg-indigo-700"}`}>Frontend</button>
+        <button name='Backend' onClick={filterDataByCategory} className={`shadow-sm shadow-white p-3 rounded cursor-pointer ${ActiveAll === "Backend" ? "bg-blue-900" : "bg-indigo-700"}`}>Backend</button>
+      </div>
+
+      <Modal opened={isDelete} onClose={() => setIsDelete(false)} title={'Are you sure?'}>
+        <Button variant="filled" onClick={handleDeleteTask} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)">Save</Button>
+        <Button variant="filled" onClick={() => setIsDelete(false)} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel</Button>
+      </Modal>
+
+      <Modal opened={iseditData} onClose={handleModelCancel} title={'Edit Your Skill'}>
+        <Input placeholder="Add Your Skills" value={skill} onChange={handleTask} name='value' />
+        <NativeSelect value={selectvalue} onChange={(ele) => { setSelectValue(ele.target.value) }} data={['Frontend', 'Backend']} className='mt-3' />
+        <Button variant="filled" onClick={updateTask} className='ms-3 mt-3' color="rgba(5, 158, 33, 1)">Update</Button>
+        <Button variant="filled" onClick={handleModelCancel} className='ms-3 mt-3' color="rgba(235, 30, 30, 1)">Cancel</Button>
+      </Modal>
       {
         categoryData.length === 0 ? (<div className="text-xl text-white font-bold mt-5">No Data Found</div>)
           : (categoryData.map((ele) => {
             return (
-              <div key={ele.id} className='p-2 mt-5 flex items-center justify-between bg-linear-60 from-blue-900 to-gray-400 shadow-xl'>{ele.skill}
+              <div key={ele.id} className='p-2 mt-8 flex items-center justify-between bg-linear-60 from-blue-900 to-gray-400 shadow-xl'>{ele.skill}
                 <div>
                   <Button variant="filled" onClick={() => { handleEditTask(ele?.id) }} className='ms-3' color="rgba(5, 158, 33, 1)"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
